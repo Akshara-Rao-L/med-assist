96faf015e33a55ad94d9b1fb57b61ec2813708a9
Akshara-Rao-L <akshararaolakkampally@gmail.com>
2026-01-01 18:47:32 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="med-assist",
+    version="0.1.0",
+    author="Akshara Rao",
+    packages=find_packages(),
+    install_requires=[]
+)
